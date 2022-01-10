6e0a93ec9c5081773e42382aee5d479735513275
kilonzi <vkilonzi05@gmail.com>
2022-01-10 10:39:42 +0300

    made model dependancies imports

--- aceinitiative/models.py
+++ aceinitiative/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.

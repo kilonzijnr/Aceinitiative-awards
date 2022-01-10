095eac5292b2bd4b3764f523ff74a53ad9172b13
kilonzi <vkilonzi05@gmail.com>
2022-01-10 10:36:36 +0300

    created urls for project directory

--- initiative/urls.py
+++ initiative/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('aceinitiative.urls')),
+    path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='home.html'), {"next_page": '/'}),
 ]

f25dcf0f64fdba5fd0b5cf9c3e18646a04088a1e
kilonzi <vkilonzi05@gmail.com>
2022-01-10 10:59:29 +0300

    made resource imports for writing app views

--- aceinitiative/views.py
+++ aceinitiative/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .models import Profile,Project,Ratings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from .forms import NewProjectForm,RegisterForm,ProfileUpdateForm,RatingsForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http.response import Http404
+
+from rest_framework.response import Response
+from .serializer import ProfileSerializer,ProjectSerializer
+from rest_framework.views import APIView
 
 # Create your views here.

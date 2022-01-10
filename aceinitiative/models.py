a35523ebbf947cc6d306a6e730128f5753f82448
kilonzi <vkilonzi05@gmail.com>
2022-01-10 10:40:29 +0300

    created profile model

--- aceinitiative/models.py
+++ aceinitiative/models.py
@@ -4,3 +4,16 @@ from cloudinary.models import CloudinaryField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Profile(models.Model):
+    """model for user profile"""
+    profile_pic = CloudinaryField("image")
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=70, blank=True)
+    email= models.EmailField()
+    bio = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()

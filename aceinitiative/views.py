ab813bde3c6b0a8e2ecfe80f3f44a41576b7a041
kilonzi <vkilonzi05@gmail.com>
2022-01-10 11:00:48 +0300

    created view for user logout

--- aceinitiative/views.py
+++ aceinitiative/views.py
@@ -42,3 +42,7 @@ def loginPage(request):
         'page':page
     }
     return render(request, 'registration/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('homepage')

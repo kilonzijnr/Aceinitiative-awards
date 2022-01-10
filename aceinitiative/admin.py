5db923c3659cec3d0cd32c9e8901681023ffc279
kilonzi <vkilonzi05@gmail.com>
2022-01-10 11:13:38 +0300

    registered model classes to admin

--- aceinitiative/admin.py
+++ aceinitiative/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Profile,Project,Ratings
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(Ratings)
